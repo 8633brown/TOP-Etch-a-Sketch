d062d6fde1b8038db1d97a53f09fb7ecb308e5e0
8633brown <toddbrown.8633@yahoo.co.uk>
2021-05-22 13:58:52 -0600

    add pointer move event listener on canvas

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+const shadePixel = function(pixel) {
+	console.log(pixel)
+}
+
 const generateGrid = function(size) {
 	const canvas = document.getElementById('canvas')
+	canvas.addEventListener('pointermove', (ev) => shadePixel(ev))
 	for (i = 0; i < size * size; i++) {
 		canvas.appendChild(document.createElement('div'))
 	}
