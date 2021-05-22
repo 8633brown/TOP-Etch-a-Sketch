8a40eba9f9dde0a7dd729d58790d4a1dad545251
8633brown <toddbrown.8633@yahoo.co.uk>
2021-05-22 14:51:16 -0600

    better grid generation and user size selection

--- script.js
+++ script.js
@@ -7,13 +7,32 @@ const shadePixel = function(pixel) {
 	pixel.style.backgroundColor = 'black'
 }
 
-const generateGrid = function(size) {
-	const canvas = document.getElementById('canvas')
-	canvas.addEventListener('pointermove', (ev) => shadePixel(ev.target))
+const generateGrid = function(size, canvas) {
+	canvas.innerHTML = ''
 	for (i = 0; i < size * size; i++) {
 		canvas.appendChild(document.createElement('div'))
 	}
+	canvas.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+	canvas.style.gridTemplateRows = `repeat(${size}, 1fr)`
 }
 
-window.addEventListener('DOMContentLoaded', () => generateGrid(16))
+const getUserCanvasSize = function() {
+	const size = Number(prompt('canvas size (must be between 16 and 64)'))
+	// fierfox seems to crash around 90, chrome can handle more.
+	if (!size || size < 16 || size > 64) {
+		return getUserCanvasSize()
+	}
+	return size
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+	const canvas = document.getElementById('canvas')
+	canvas.addEventListener('pointermove', (ev) => shadePixel(ev.target))
+	generateGrid(16, canvas)
+
+	document.getElementById('newCanvas').addEventListener('click', () => {
+		const size = getUserCanvasSize()
+		generateGrid(size, canvas)
+	})
+})
 
